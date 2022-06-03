086218410a54e34b8a4a29be59d254b0a80336e7
lmitelman <lautaromitelman@gmail.com>
2022-06-03 20:04:20 -0300

    class consorcio

--- src/modelo/Consorcio.java
+++ src/modelo/Consorcio.java
@@ -4,8 +4,12 @@ public class Consorcio {
 
     private String nombreConsorcio;
     private UnidadFuncional[] unidadesFuncionales;
+    // adaptercuentaBancaria: IAdapterSaldo
+    // estrategiaEnvio: IObserverEnvio
+    // estrategiaPago: AbstractEstrategiaPago
     private Gasto[] gasto;
-    private String operador;
+    private Double saldoActual;
+    // private Operador operador;
 
     public Consorcio(String nombreConsorcio) {
         this.nombreConsorcio = nombreConsorcio;
@@ -14,5 +18,23 @@ public class Consorcio {
     public String getNombreConsorcio() {
         return nombreConsorcio;
     }
+
+    // public static void agregarOperador(Operador operador) {}
+
+    // public static void eliminarOperador(Operador operador) {}
+    
+    // public static Double obtenerSaldoAtual() {
+    //     return 3.0;
+    // }
+
+    // public static void generarExpensa(UnidadFuncional unidadFuncional, AbstractEstrategiaPago estrategia) {}
+
+    // public static void cambioEstrategiaPago(AbstractEstrategiaPago nuevaEstrategia) {}
+
+    // public static void agregarObservador(UnidadFuncional unidadFuncional) {}
+
+    // public static void eliminarObservador(UnidadFuncional unidadFuncional) {}
+
+    // public static void cambioEstrategiaEnvio(AbstractEstrategiaPago nuevaEstrategia) {}
 
 }
